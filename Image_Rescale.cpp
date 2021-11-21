228e4b8d2449757336f6cac3202acf71daaa32fa
Maninder-sd <maninder.123.456@live.com>
2021-11-21 03:52:24 -0500

    simplified formula + get pixel indexing

--- Image_Rescale.cpp
+++ Image_Rescale.cpp
@@ -146,6 +146,7 @@ unsigned char *fast_rescaleImage(unsigned char *src, int src_x, int src_y, int d
 {
  double step_x,step_y;			// Step increase as per instructions above
  unsigned char RGB[12];     // single allocation for all variables R1,G1,B1,R2...
+ int indexes[12];
 //  unsigned char R1,R2,R3,R4;		// Colours at the four neighbours
 //  unsigned char G1,G2,G3,G4;
 //  unsigned char B1,B2,B3,B4;
@@ -187,35 +188,40 @@ for(i=0; __builtin_expect ( i<(dest_x*dest_y) ,1 );i+=3)
    // these will reduce casting 
    floor_fx = fx;
    floor_fy = fy;
-
-   
-   ceil_fx = (fx == floor_fx) ? floor_fx : floor_fx+1;
-   ceil_fy = (fy == floor_fy) ? floor_fy  : floor_fy+1;
-
    dx=fx-floor_fx;
-   dy=fy-floor_fy;   
+   dy=fy-floor_fy; 
+   
+   ceil_fx = (fx == floor_fx) ? 3*floor_fx : 3*(floor_fx+1);
+   ceil_fy = (fy == floor_fy) ? 3*floor_fy *src_x : 3*(floor_fy+1)*src_x;
+  //  ceil_fx = (fx == floor_fx) ? 3*floor_fx : 3*(floor_fx+1);
+  //  ceil_fy = (fy == floor_fy) ? 3*floor_fy  : 3*(floor_fy+1);
+   floor_fx *=3;
+   floor_fy *=3*src_x;
+
+
+  
 
 
 
   //  getPixel(src,floor(fx),floor(fy),src_x,&R1,&G1,&B1);	// function calls are expensive
-   RGB[0]=*(src+(( floor_fx + (floor_fy*src_x))*3)+0); //R1
-   RGB[1]=*(src+(( floor_fx + (floor_fy*src_x))*3)+1); //G1 
-   RGB[2]=*(src+(( floor_fx + (floor_fy*src_x))*3)+2); //B1
+   RGB[0]=*(src+( floor_fx + (floor_fy))+0); //R1
+   RGB[1]=*(src+( floor_fx + (floor_fy))+1); //G1 
+   RGB[2]=*(src+( floor_fx + (floor_fy))+2); //B1
 
   //  getPixel(src,ceil(fx),floor(fy),src_x,&R2,&G2,&B2);	// get N2 colours
-   RGB[3]=*(src+(( ceil_fx + (floor_fy*src_x))*3)+0);  //R2
-   RGB[4]=*(src+(( ceil_fx + (floor_fy*src_x))*3)+1);  //G2
-   RGB[5]=*(src+(( ceil_fx + (floor_fy*src_x))*3)+2);  //B2
+   RGB[3]=*(src+( ceil_fx + (floor_fy))+0);  //R2
+   RGB[4]=*(src+( ceil_fx + (floor_fy))+1);  //G2
+   RGB[5]=*(src+( ceil_fx + (floor_fy))+2);  //B2
 
   //  getPixel(src,floor(fx),ceil(fy),src_x,&R3,&G3,&B3);	// get N3 colours
-   RGB[6]=*(src+(( floor_fx + (ceil_fy*src_x))*3)+0); //R3
-   RGB[7]=*(src+(( floor_fx + (ceil_fy*src_x))*3)+1); //G3
-   RGB[8]=*(src+(( floor_fx + (ceil_fy*src_x))*3)+2); //B3
+   RGB[6]=*(src+( floor_fx + (ceil_fy))+0); //R3
+   RGB[7]=*(src+( floor_fx + (ceil_fy))+1); //G3
+   RGB[8]=*(src+( floor_fx + (ceil_fy))+2); //B3
 
   //  getPixel(src,ceil(fx),ceil(fy),src_x,&R4,&G4,&B4);	// get N4 colours
-   RGB[9]=*(src+(( ceil_fx + (ceil_fy*src_x))*3)+0);  //R4
-   RGB[10]=*(src+(( ceil_fx + (ceil_fy*src_x))*3)+1); //G4
-   RGB[11]=*(src+(( ceil_fx + (ceil_fy*src_x))*3)+2);  //B4
+   RGB[9]=*(src+( ceil_fx + (ceil_fy))+0);  //R4
+   RGB[10]=*(src+( ceil_fx + (ceil_fy))+1); //G4
+   RGB[11]=*(src+( ceil_fx + (ceil_fy))+2);  //B4
 
    // Interpolate to get T1 and T2 colours
   //  RT1=((dx*RGB[3])+(1-dx)*RGB[0]);
@@ -225,11 +231,23 @@ for(i=0; __builtin_expect ( i<(dest_x*dest_y) ,1 );i+=3)
   //  GT2=((dx*RGB[10])+(1-dx)*RGB[7]);
   //  BT2=((dx*RGB[11])+(1-dx)*RGB[8]);
    // Obtain final colour by interpolating between T1 and T2
-   R=(unsigned char)((dy*((dx*RGB[9])+(1-dx)*RGB[6]))+((1-dy)*((dx*RGB[3])+(1-dx)*RGB[0])));
-   G=(unsigned char)((dy*((dx*RGB[10])+(1-dx)*RGB[7]))+((1-dy)*((dx*RGB[4])+(1-dx)*RGB[1])));
-   B=(unsigned char)((dy*((dx*RGB[11])+(1-dx)*RGB[8]))+((1-dy)*((dx*RGB[5])+(1-dx)*RGB[2])));
+  //  R=(unsigned char)((dy*((dx*RGB[9])+(1-dx)*RGB[6]))+((1-dy)*((dx*RGB[3])+(1-dx)*RGB[0])));
+  //  G=(unsigned char)((dy*((dx*RGB[10])+(1-dx)*RGB[7]))+((1-dy)*((dx*RGB[4])+(1-dx)*RGB[1])));
+  //  B=(unsigned char)((dy*((dx*RGB[11])+(1-dx)*RGB[8]))+((1-dy)*((dx*RGB[5])+(1-dx)*RGB[2])));
+
+  //simplified formulas to reduce multiplications
+   R=(unsigned char)(  dx*dy*(RGB[9] -RGB[6] -RGB[3] -RGB[0] ) + dy*(RGB[6] -RGB[0]) + dx*(RGB[3] -RGB[0]) + RGB[0]  ); 
+   G=(unsigned char)(  dx*dy*(RGB[10] -RGB[7] -RGB[4] -RGB[1] ) + dy*(RGB[7] -RGB[1]) + dx*(RGB[4] -RGB[1]) + RGB[1]  );
+   B=(unsigned char)(  dx*dy*(RGB[11] -RGB[8] -RGB[5] -RGB[2] ) + dy*(RGB[8] -RGB[2]) + dx*(RGB[5] -RGB[2]) + RGB[2]  );
+
+
+  //  R=(unsigned char)((dy*RT2)+((1-dy)*RT1));
+  //  G=(unsigned char)((dy*GT2)+((1-dy)*GT1));
+  //  B=(unsigned char)((dy*BT2)+((1-dy)*BT1));
    // Store the final colour
   //  setPixel(dst,x,y,dest_x,R,G,B);
+
+  // using i so no need to calculate index
    *(dst+(i)+0)=R;
    *(dst+(i)+1)=G;
    *(dst+(i)+2)=B;
